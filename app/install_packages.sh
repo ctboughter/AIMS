49ed3217b83692cc77e3b873b6bd230372727ac9
Christopher Boughter <ctboughter@gmail.com>
2020-11-10 18:58:53 -0500

    Update install_packages.sh

--- app/install_packages.sh
+++ app/install_packages.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-conda install -c conda-forge kivy
+# Note to advanced users, only exact Biopython and Scikit-Learn versions should be necessary
+# All other packages should be compatible with multiple versions, but have not been tested
+
+conda install -c conda-forge kivy=1.11.1
 conda install -c conda-forge biopython=1.76
-conda install -c conda-forge scipy
-conda install pandas
-conda install numpy
-conda install matplotlib
-conda install scikit-learn
-conda install seaborn
+conda install -c conda-forge scipy=1.4.1
+conda install pandas=1.0.3
+conda install numpy=1.18.1
+conda install matplotlib=3.1.3
+conda install scikit-learn=0.22.1
+conda install seaborn=0.10.1
